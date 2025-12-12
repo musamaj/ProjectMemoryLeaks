973aa8fe10e4835e5ee34ba46ba11f433a9bc92e
Usama Jamil <usama.jamil@ext.beyond.one>
2025-12-12 00:23:43 -0500

    Initial Commit

---
+++ TestLeaks/TestLeaksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLeaksApp.swift
+//  TestLeaks
+//
+//  Created by Muhammad Usama Jamil on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TestLeaksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
